0a267faecbaf686293980ccebceb1f8ce83bd801
William McGonagle <wpmcgonagle@gmail.com>
2022-04-10 13:01:07 -0400

    Testing in Production!!!

--- src/routes/user/questions.js
+++ src/routes/user/questions.js
@@ -3,9 +3,10 @@ module.exports = (req, res) => {
     if (req.params.id == undefined) return res.status(400).json({ error: "Not All Parameters Provided." });
 
     Question.findAll({
-        attributes: [ 'id', 'title', 'body', 'user', [sequelize.fn('count', sequelize.col('Answer.id')), 'answers'] ],
-        include: [{ attributes: [], model: Answer, required: true, duplicating: false }],
+        include: [{ as: 'Answer', model: Answer, attributes: [], /* required: true, duplicating: false */ }],
+        attributes: [ 'id', 'title', 'body', 'user', [sequelize.fn('count', sequelize.col('Answer.id')), 'total'] ],
         group: ['Question.id'],
+
         where: {
             user: req.params.id
         }
