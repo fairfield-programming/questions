62b22dd0333c2a4c2d9c40da913fe3136720726a
William McGonagle <wpmcgonagle@gmail.com>
2022-04-10 13:15:15 -0400

    Fixed Related

--- src/routes/question/related.js
+++ src/routes/question/related.js
@@ -2,7 +2,17 @@ module.exports = (req, res) => {
 
     if (req.params.id == undefined) return res.status(400).json({ error: "Not All Parameters Provided." });
 
-    Question.findMany({
+    Question.findAll({
+
+        attributes: { 
+            include: [[Sequelize.fn("COUNT", Sequelize.col("Answers.id")), "answers"], [Sequelize.fn("COUNT", Sequelize.col("Comments.id")), "comments"]] 
+        },
+        include: [{
+            model: Answer, attributes: []
+        }, {
+            model: Comment, attributes: []
+        }],
+        group: ['Question.id'],
 
     }).then(function (data) {
 
