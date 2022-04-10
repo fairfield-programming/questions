10d20f52ea8d3580fbad9931152213dcb842735b
William McGonagle <wpmcgonagle@gmail.com>
2022-04-10 13:48:01 -0400

    Fixed Question Posting

--- src/routes/question/create.js
+++ src/routes/question/create.js
@@ -1,11 +1,13 @@
 module.exports = (req, res) => {
 
     if (req.body.user == undefined) return res.status(400).json({ error: "Not All Body Items Provided." });
-    if (req.body.text == undefined) return res.status(400).json({ error: "Not All Body Items Provided." });
+    if (req.body.title == undefined) return res.status(400).json({ error: "Not All Body Items Provided." });
+    if (req.body.body == undefined) return res.status(400).json({ error: "Not All Body Items Provided." });
 
     Question.create({
         user: req.body.user,
-        body: req.body.text
+        title: req.body.title,
+        body: req.body.body
     }).then(function (answerData) {
 
         return res.status(200).json(answerData);
