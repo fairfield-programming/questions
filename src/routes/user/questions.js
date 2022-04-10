9394859c4035d47acd2dc4d10f53ce62264e5bbe
William McGonagle <wpmcgonagle@gmail.com>
2022-04-10 12:26:40 -0400

    Adding Feature

--- src/routes/user/questions.js
+++ src/routes/user/questions.js
@@ -3,13 +3,27 @@ module.exports = (req, res) => {
     if (req.params.id == undefined) return res.status(400).json({ error: "Not All Parameters Provided." });
 
     Question.findAll({
+        attributes: [
+            'id',
+            'title',
+            'body',
+            { 
+                include: [[Sequelize.fn('COUNT', Sequelize.col('Comment.questionId')), 'comments']] 
+            },
+            { 
+                include: [[Sequelize.fn('COUNT', Sequelize.col('Answer.questionId')), 'answers']] 
+            }
+        ],
+        include: [{
+            model: Answer, attributes: []
+        }, {
+            model: Comment, attributes: []
+        }],
         where: {
             user: req.params.id
         }
     }).then(function (data) {
 
-        // if (data == null) return res.status(404).json({ error: "Question Not Found." });
-        
         return res.status(200).json(data);
 
     }).catch(function (error) {
